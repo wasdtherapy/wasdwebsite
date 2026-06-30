49dd3ee3ce407c4dd5d97b3dc9cd9c3ff65d28d6
wasdtherapy <davidvirus228@gmail.com>
2026-07-01 01:43:28 +0300

    Delete wasdtherapy_soty_nextjs directory

--- wasdtherapy_soty_nextjs/components/ThemeVars.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useEffect } from "react";
-import { useStore } from "@/lib/store";
-
-export default function ThemeVars() {
-  const palette = useStore((s) => s.palette);
-  useEffect(() => { document.documentElement.dataset.pal = palette; }, [palette]);
-  return null;
-}
