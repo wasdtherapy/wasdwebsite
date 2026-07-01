01d62f0203a7d88a92527f7c1ae5faac883cc26c
wasdtherapy <davidvirus228@gmail.com>
2026-07-01 12:29:12 +0300

    Delete wasdtherapy_soty_nextjs directory

--- wasdtherapy_soty_nextjs/app/sounds/layout.tsx
+++
@@ -1,8 +0,0 @@
-import type { Metadata } from "next";
-import { sectionMeta } from "@/lib/seo";
-
-export const metadata: Metadata = sectionMeta("sounds");
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return children;
-}
